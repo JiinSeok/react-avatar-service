1973412d4b91c8c98a41e53115306eed0f8dcfb9
JiinSeok <seokjiin1073@gmail.com>
2024-08-29 23:35:29 +0900

    에러 리스폰스 코드 401일 때 보낸 리퀘스트 다시 보냄

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -5,4 +5,15 @@ const instance = axios.create({
   withCredentials: true,
 });
 
+
+instance.interceptors.response.use(res => res, async (error) => {
+  const originalRequest = error.config;
+  const response = error.response; // 가로 챈 리스폰스
+  if (error.response?.status === 401 && !originalRequest._retry) {
+    originalRequest._retry = true;
+    return instance(originalRequest);
+  }
+  return Promise.reject(error);
+});
+
 export default instance;
